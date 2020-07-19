bbb4aab86fd2e73eeceee64e92d50d25d85c7ee7
takara2314 <takara2314_TK@outlook.jp>
2020-07-20 04:01:39 +0900

    🚀 Release v0.1

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 
 func main() {
 	// 毎時指定した時間に TCJ2 Kadai Store API から課題一覧を取得
-	go getRegularly([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59})
+	go getRegularly([]int{1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56})
 
 	r := gin.Default()
 
 	r.GET("/", homeRequestFunc)
 	r.GET("/line-callback", lineRequestFunc)
 
-	r.Run()
+	r.Run(":8080")
 }
 
 // homeRequestFunc は/アクセスされたときの処理
